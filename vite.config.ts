feea02ee9fcf116fa70c2349bd8ee843e22960d0
vimalramk <vimalramk99@gmail.com>
2024-08-07 18:22:01 +0530

    fix : fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,18 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: ["./src/setupTests.js"],
   },
+  build: {
+    outDir: "./dist",
+  },
+  
 });
